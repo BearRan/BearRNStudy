d40913ff4a0077bc5cc15e47a9bea46c09d87df4
Ran Xiong <ran.xiong@yuntu-inc.com>
2017-07-12 22:27:01 +0800

    继续学习

--- BearRNTest2/DemoFile/2-1StudyRNNav.ios.js
+++ BearRNTest2/DemoFile/2-1StudyRNNav.ios.js
@@ -19,7 +19,7 @@ class HomeScreen extends React.Component {
         const { navigate } = this.props.navigation;
         return (
         <View>
-        <Text>Hello, Chat App!</Text>
+            <Text>Hello, Chat App!</Text>
             <Button
                 onPress={() => navigate('Chat')}
                 title="Chat with Lucy"
@@ -34,11 +34,30 @@ class ChatScreen extends React.Component {
         title: 'Chat with Lucy',
     };
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View>
-            <Text>Chat with Lucy</Text>
-        </View>
-            );
+                <Text>Chat with Lucy</Text>
+                <Button
+                    onPress={() => navigate('Chat1', { user: 'Lucy' })}
+                    title="Chat with Lucy1"
+                />
+            </View>
+        );
+    }
+}
+
+class ChatScreen1 extends React.Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.user}`,
+    });
+    render() {
+        const { params } = this.props.navigation.state;
+        return (
+            <View>
+                <Text>Chat with {params.user}</Text>
+            </View>
+        );
     }
 }
 
@@ -46,6 +65,7 @@ class ChatScreen extends React.Component {
 const SimpleApp = StackNavigator({
     Home: {screen: HomeScreen},
     Chat: { screen: ChatScreen },
+    Chat1: { screen: ChatScreen1 },
 });
 
 AppRegistry.registerComponent('BearRNTest2', () => SimpleApp);
